7d2f310476d308d0b0c06c92b89c1dcd9fa711aa
anhlt <anhlt.bvh@gmail.com>
2021-12-03 00:25:36 +0700

    update

--- testApi/src/main/java/Common/BaseRestAssured.java
+++ testApi/src/main/java/Common/BaseRestAssured.java
@@ -9,13 +9,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BaseRestAssured {
-    public static final String TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyI2MTI1YzEwOWMxZWM4YjAwMWI3Y2NlNzMiLCI2MTlmNThjMzk2M2NlYmUxMDIxZTlhNDUiLCI2MTI1YzBkY2MxZWM4YjAwMWI3Y2NlNmUiXSwiZ3JvdXBzIjpbXSwiX2lkIjoiNjFhODczNDlhYjFlMWMwMDE0MGIxNWIzIiwiZXhwaXJlZEF0IjoxNjcyNTA1OTk5MDAwLCJuYW1lIjoiYWRhc2RhcyIsImFwcGxpY2F0aW9uIjoiIiwibWVyY2hhbnRJZCI6IjYwZWU2MmFhNDM0MjQwMDAxMzkxYjgwMyIsInVpZCI6IjRlM2UxMGRmLWY4OTAtNDY4OC05ODhiLWM4YmY5YjVjOThkNSIsImFjdGl2YXRlZCI6MSwiY3JlYXRlZEJ5IjoiNjEzOTA1N2Y5YTdjMDUwMDEyZGY4MmY4IiwiY3JlYXRlZCI6MTYzODQyOTUxMzA2MiwiX192IjowLCJpYXQiOjE2Mzg0Mjk1MTMsImV4cCI6MTY3MjQ3MTExM30.ixPSOuqlyhUx8nl87HJJk2HcFDZRBCBMtyilmcb9dGw";
+    public static final String TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyI2MTI1YzEwOWMxZWM4YjAwMWI3Y2NlNzMiLCI2MTlmNThjMzk2M2NlYmUxMDIxZTlhNDUiLCI2MTI1YzBkY2MxZWM4YjAwMWI3Y2NlNmUiXSwiZ3JvdXBzIjpbXSwiX2lkIjoiNjFhODczNDlhYjFlMWMwMDE0MGIxNWIzIiwiZXhwaXJlZEF0IjoxNjcyNTA1OTk5MDAwLCJuYW1lIjoiYWRhc2RhcyIsImFwcGxpY2F0aW9uIjoiIiwibWVyY2hhbnRJZCI6IjYwZWU2MmFhNDM0MjQwMDAxMzkxYjgwMyIsInVpZCI6IjRlM2UxMGRmLWY4OTAtNDY4OC05ODhiLWM4YmY5YjVjOThkNSIsImFjdGl2YXRlZCI6MSwiY3JlYXRlZEJ5IjoiNjEzOTA1N2Y5YTdjMDUwMDEyZGY4MmY4IiwiY3JlYXRlZCI6MTYzODQyOTUxMzA2MiwiX192IjowLCJpYXQiOjE2Mzg0Mjk1MTMsImV4cCI6MTY3MjQ3MTExM30.ixPSOuqlyhUx8nl87HJJk2HcFDZRBCBMtyilmcb9dGw";
     public static final Map<String, String> HEADER = new HashMap<String, String>() {{
-        put("ContentType","application/json");
+        put("ContentType", "application/json");
         put("x-access-token", TOKEN);
     }};
 
-    public static JsonPath callPostMethod(String url, String body,File file) {
+    public static JsonPath callPostMethod(String url, String body, File file) {
         return RestAssured.given()
                 .headers(HEADER)
                 .body(body)
@@ -29,7 +29,7 @@ public class BaseRestAssured {
                 .jsonPath();
     }
 
-    public static JsonPath callGetMethod(String url, Map<String,?> param) {
+    public static JsonPath callGetMethod(String url, Map<String, ?> param) {
         return RestAssured.given()
                 .queryParams(param)
                 .headers(HEADER)
@@ -43,7 +43,7 @@ public class BaseRestAssured {
                 .jsonPath();
     }
 
-    public static JsonPath callPutMethod(String url, JsonObject body,File file) {
+    public static JsonPath callPutMethod(String url, JsonObject body, File file) {
         return RestAssured.given()
                 .headers(HEADER)
                 .body(body)
@@ -59,16 +59,34 @@ public class BaseRestAssured {
     }
 
     public static void main(String[] args) {
-        String url = "https://gateway-dev.nextcam.vn/dev-api/camera";
-        Map<String, ?> param = new HashMap<String,Object>(){{
-            put("page",1);
-            put("perPage",1);
-            put("filter","");
-            put("fromDate","");
-            put("toDate","");
-            put("sort","");
-        }};
-        System.out.println(callGetMethod(url,param).prettyPrint());
+//        String url = "https://gateway-dev.nextcam.vn/dev-api/camera";
+//        Map<String, ?> param = new HashMap<String,Object>(){{
+//            put("page",1);
+//            put("perPage",1);
+//            put("filter","");
+//            put("fromDate","");
+//            put("toDate","");
+//            put("sort","");
+//        }};
+//        System.out.println(callGetMethod(url,param).prettyPrint());
+        /*upfile*/
+        String url = "https://cloud-upload-dev.nextcam.vn/upload";
+        File file = new File("src/main/resources/image/1.PNG");
+        String body = "{\"file\":\"file\"}";
+        JsonPath res = RestAssured.given()
+                .log().all()
+                .headers("Content-Type", "multipart/form-data")
+                .body(body)
+                .multiPart("file",file)
+                .when()
+                .log().all()
+                .post(url)
+                .then()
+                .log().all()
+                .extract()
+                .response()
+                .jsonPath();
+        System.out.println(res.prettyPrint());
 
     }
 }
--- testApi/src/main/java/stepsdefinitions/Location/CreateLocation.java
+++ testApi/src/main/java/stepsdefinitions/Location/CreateLocation.java
@@ -16,47 +16,21 @@ import static net.serenitybdd.core.Serenity.setSessionVariable;
 public class CreateLocation {
     @Given("Setup data pre call name {},desc {}, address {}, zoneId {}")
     public void setUp(String name, String desc, String address, String zoneId) {
-//        String body = "{\n" +
-//                "  \"name\": \""+name+"\",\n" +
-//                "  \"description\": \""+desc+"\",\n" +
-//                "  \"address\": \""+address+"\",\n" +
-//                "  \"zoneIds\": [\n" +zoneId+
-//                "  ]\n" +
-//                "}";
         String url = "https://gateway-dev.nextcam.vn/dev-api/location";
         setSessionVariable("endpoint").to(url);
         JsonObject jsonBody = JsonParser.parseString(BodyJson.readTextFile("src/main/resources/payloads/createLocation.json")).getAsJsonObject();
         jsonBody.addProperty("name", name);
         jsonBody.addProperty("description", desc);
         jsonBody.addProperty("address", address);
-//        String[] zoneIds = zoneId.split(",");
-//        JsonArray zone = jsonBody.get("zoneIds").getAsJsonArray();
-//        for (String zone1: zoneIds) {
-//            zone.add(zone1);
-//        }
-//        jsonBody.add("zoneIds",  zoneIds);
         setSessionVariable("body").to(jsonBody.toString());
     }
 
     @When("Call API")
     public void callAPI() {
-//        System.out.println((JsonObject) sessionVariableCalled("body"));
-        JsonObject body = sessionVariableCalled("body");
-//        JsonPath resp = RestAssured
-//                .given()
-//                    .headers("x-access-token", BaseRestAssured.TOKEN)
-//                    .body(body.toString())
-//                    .baseUri(sessionVariableCalled("endpoint"))
-//                .when()
-//                    .post()
-//                .then()
-//                    .extract()
-//                    .response()
-//                    .jsonPath();
-//
-//        setSessionVariable("res").to(resp);
-//        System.out.println(resp.prettyPrint());
-       JsonPath res =  BaseRestAssured.callPostMethod(sessionVariableCalled("endpoint").toString(),sessionVariableCalled("body"),null);
+        String body = sessionVariableCalled("body");
+        System.out.println(body);
+        System.exit(1);
+       JsonPath res =  BaseRestAssured.callPostMethod(sessionVariableCalled("endpoint").toString(),body,null);
        setSessionVariable(res);
         System.exit(1);
     }
